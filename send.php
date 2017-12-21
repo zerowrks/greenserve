21d29a041da3191af5f0a74e607f78ebfb3cc18c
zerowrks <34137954+zerowrks@users.noreply.github.com>
2017-12-21 22:41:57 +0530

    Add files via upload

---
+++ send.php
@@ -0,0 +1,83 @@
+<?php 
+if(isset($_POST['submit']) && ($_POST['submit']=='Send')):	
+	$name=trim($_POST['name'],"  ");
+	$to="jacobvee@gmail.com";
+	$email = trim($_POST['email'],"  ");
+	$product_code = trim($_POST['product'],"  ");
+	$subject= trim($_POST['subject'],"  ");
+	$enquiry = trim($_POST['message'],"  ");
+    $message = '<!DOCTYPE HTML>'.
+'<head>'.
+'<meta http-equiv="content-type" content="text/html">'.
+'<title>Email Notification</title>'.
+'</head>'.
+'<body>'.
+'<div id="outer" style="width: 80%;margin: 0 auto;margin-top: 10px;">'. 
+   '<div id="inner" style="width: 78%;margin: 0 auto;background-color: #fff;font-family: Open Sans,Arial,sans-serif;font-size: 13px;font-weight: normal;line-height: 1.4em;color: #444;margin-top: 10px;">'.
+       '<p>Name : ' .$name.'</p>'.
+       '<p>Email : ' .$email.'</p>'.
+	   '<p>Product Code : ' .$product_code.'</p>'.
+	   '<p>Subject : ' .$subject.'</p>'.
+       '<p>Message : ' .$enquiry.'</p>'.
+   '</div>'.  
+'</div>'.
+'</body>';
+
+/*EMAIL TEMPLATE ENDS*/ 
+
+	$headers = "MIME-Version: 1.0" . "\r\n";
+	$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+	$headers .= 'From: <jacobvee@gmail.com>' . "\r\n";
+
+	
+	$alert = mail($to,$subject,$message,$headers);
+	
+   if($alert) : echo 1;
+   else: echo 0;
+   endif;
+
+  else:
+  
+  	$name=trim($_POST['name'],"  ");
+	$to="jacobvee@gmail.com";
+	$email = trim($_POST['email'],"  ");
+	$subject= trim($_POST['subject'],"  ");
+	$enquiry = trim($_POST['message'],"  ");
+    $message = '<!DOCTYPE HTML>'.
+'<head>'.
+'<meta http-equiv="content-type" content="text/html">'.
+'<title>Email Notification</title>'.
+'</head>'.
+'<body>'.
+'<div id="outer" style="width: 80%;margin: 0 auto;margin-top: 10px;">'. 
+   '<div id="inner" style="width: 78%;margin: 0 auto;background-color: #fff;font-family: Open Sans,Arial,sans-serif;font-size: 13px;font-weight: normal;line-height: 1.4em;color: #444;margin-top: 10px;">'.
+       '<p>Name : ' .$name.'</p>'.
+       '<p>Email : ' .$email.'</p>'.
+	   '<p>Subject : ' .$subject.'</p>'.
+       '<p>Message : ' .$enquiry.'</p>'.
+   '</div>'.  
+'</div>'.
+'</body>';
+
+/*EMAIL TEMPLATE ENDS*/ 
+
+	$headers = "MIME-Version: 1.0" . "\r\n";
+	$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+	$headers .= 'From: <jacobvee@gmail.com>' . "\r\n";
+
+	
+	$alert = mail($to,$subject,$message,$headers);
+	
+   if($alert) : echo 1;
+   else: echo 0;
+   endif;
+  
+  endif;
+  
+  
+  
+  
+
+
+
+
